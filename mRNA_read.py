02819a0f1cb847a538b2cc717f2451bc42651071
Brooke Anne Jennings <brookeannej@icloud.com>
2018-01-10 20:12:01 -0600

    Update mRNA analysis script.

--- mRNA_read.py
+++ mRNA_read.py
@@ -1,12 +1,8 @@
-# DELETE THIS:
-# test
-# 10 C's, 10 G's
-text = "GCCGCCGCGCGGGCGCGCCGC"
-characters = list(text)
-#
-#
+import csv
 import sys
-# read arguments in a better way
+
+# TODO: read arguments in a better way (argparse)
+# TODO: add tests
 
 if len(sys.argv) < 2:
     print 'Need to pass the name of the file'
@@ -14,26 +10,46 @@ if len(sys.argv) < 2:
 else:
     filename = sys.argv[1]
 
-text = open(filename).read()
-##print text
-characters = list(text)
-##print characters
+lines = open(filename).readlines()
+lines = [l.strip('\n') for l in lines]
 
-C_count = 0
-G_count = 0
-CG_count = 0
+genes = {}
 
-for i in range(len(characters)-1):
-    if characters[i] == "C":
-        C_count += 1
-    elif characters[i] == "G":
+id_ = ""
+name = ""
+sequence = ""
+
+for line in lines:
+    if line[0] == '>':
+        id_ = line.split(' ')[0].strip('>')
+        name = " ".join(line.split(',')[0].split(' ')[1])
+        genes[id_] = {}
+        genes[id_]['name'] = name
+        genes[id_]['sequence'] = ""
+    else:
+        genes[id_]['sequence'] += line
+
+for g, v in genes.items():
+    C_count = 0
+    G_count = 0
+    CG_count = 0
+    seq = v['sequence']
+    for i in range(len(seq)-1):
+        if seq[i] == "C":
+            C_count += 1
+        elif seq[i] == "G":
+            G_count += 1
+        if seq[i] + seq[i+1] == 'CG':
+            CG_count += 1
+
+    if seq[-1] == 'G':
         G_count += 1
-    if characters[i] + characters[i+1] == 'CG':
-        CG_count += 1
+    elif seq[-1] == 'C':
+        C_count += 1
+    genes[g]['C_count'] = C_count
+    genes[g]['G_count'] = G_count
+    genes[g]['CG_count'] = CG_count
 
-if characters[-1] == 'G':
-    G_count += 1
-elif characters[-1] == 'C':
-    C_count += 1
-
-print "c count:", C_count, "g count:", G_count, "CG count", CG_count
+with open('mRNA_analysis.csv','wb') as f:
+    w = csv.writer(f)
+    w.writerows(genes.items())
